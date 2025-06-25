7d17a6f6d14028656a2febda7d1ff7620f91699d
william11074 <william1324230@gmail.com>
2025-06-25 13:53:15 -0700

    Co-authored-by: toothlessfire <toothlessfire@users.noreply.github.com>

--- user_creation_test.py
+++ user_creation_test.py
@@ -142,14 +142,24 @@ def create_user():
     if request.method == 'POST':
         try:
             with Session(engine) as session:
-                if (request.form.get('user_type') == 'student'):
-                    user = Student(
-                        user_type='Student'
-                    )
-                    session.add(user)
+                user = User(
+                    user_type=request.form.get('user_type'),
+                    name = request.form.get('name'),
+                    district = request.form.get('district'),
+                    age = request.form.get('age')
+                )
+                session.add(user)
+                session.commit()
+                user2 = User_Login(id=user.id, username=request.form.get('username'), password=request.form.get('password'))
+                session.add(user2)
+                session.commit()
+                if request.form.get('user_type') == 'Tutor':
+                    tutor = Tutor(id=user.id, subjects=request.form.get('subjects'))
+                    session.add(tutor)
                     session.commit()
-                    user2 = User_Login(id=user.id, username=request.form.get('username'), password=request.form.get('password'))
-                    session.add(user2)
+                elif request.form.get('user_type') == 'Student':
+                    student = Student(id=user.id, teacher_id=request.form.get('teacher_id'), tutor_id=request.form.get('tutor_id'), grade=request.form.get('grade'), stored_chats=request.form.get('stored_chats'), staring_assessment=request.form.get('staring_assessment'), current_subject=request.form.get('current_subject'), progress_percentage=request.form.get('progress_percentage'))
+                    session.add(student)
                     session.commit()
             return f"User created successfully!"
         except Exception as e:
